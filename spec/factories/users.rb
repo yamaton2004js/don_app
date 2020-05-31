66241fd080c22c7a23f9c6d22d58d4a377a5df7c
yamaton2004js <b07.lucky.boy26@docomo.ne.jp>
2020-05-31 14:52:38 +0900

    testcode user

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name                     {"yamada"}
+    email                    {"s@gmail.com"}
+    password                 {"000000"}
+    password_confirmation    {"000000"}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,45 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
-    
-  
-  end
 
+    it 'name,email,password,password_confirmationが存在すれば登録できること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'nameがない場合は登録できないこと' do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it 'emailがない場合は登録できないこと' do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it 'passwordがない場合は登録できないこと' do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it 'passwordpassが存在してもword_confirmationがない場合は登録できないこと' do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("don't match password")
+    end
+
+    it 'passwordが6文字以上であれば登録できること' do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      expect(user).to be_valid
+    end
+
+    it 'passwordが5文字以下であれば登録できないこと' do
+      user = build(:user, password: "00000", password_confirmation: "00000")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    end
+  end
 end
